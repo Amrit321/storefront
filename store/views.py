a92cf4f45427cc0ec4c0be5aaab3b2f9fc5423af
amrit <peaceamrit95@gmail.com>
2025-09-13 10:32:33 +0545

    delete method api

--- store/views.py
+++ store/views.py
@@ -25,17 +25,27 @@ def product_list(request):
         
              
 
-@api_view(['GET', 'PUT'])
+@api_view(['GET', 'PUT', 'DELETE'])
 def product_detail(request, id):
         product = get_object_or_404(Product, pk=id)
+        # retriving the data
         if request.method == 'GET':
             serializer = ProductSerializer(product)
             return Response(serializer.data)
+        # to update the product
         elif request.method == 'PUT':
             serializer = ProductSerializer(product, data = request.data)
             serializer.is_valid(raise_exception=True)
             serializer.save()
             return Response(serializer.data)
+        elif request.method == 'DELETE':
+            # here orderitems is related name in model OrderItem 
+            if product.orderitems.count() > 0:
+                  return Response({'error': 'Product cannot be deleted because it is associated with order item'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
+            product.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)        
+
+
 @api_view()
 def collection_detail(request, pk):
       return Response('ok')
