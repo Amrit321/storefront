cb8a8deaeb44072457163352937122960fd87353
amrit <peaceamrit95@gmail.com>
2025-09-14 16:49:05 +0545

    converting to class based views

--- store/urls.py
+++ store/urls.py
@@ -2,9 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('products/', views.product_list),
+    path('products/', views.ProductList.as_view()),
+     path('collections/<int:id>/', views.ProductDetail.as_view()),
+    # path('products/', views.product_list),
     path('collections/', views.collection_list),
-    path('products/<int:id>/', views.product_detail),
+    # path('products/<int:id>/', views.product_detail),
     path('collections/<int:id>/', views.collection_detail, name='collection-detail')
 ]
         
--- store/views.py
+++ store/views.py
@@ -1,50 +1,94 @@
 from django.db.models import Count
 from django.shortcuts import get_object_or_404
 from django.http import HttpResponse
+from rest_framework.mixins import ListModelMixin, CreateModelMixin
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.views import APIView
 from rest_framework import status
 from .models import Product, Collection
 from .serializers import ProductSerializer, CollectionSerializer
 # Create your views here.
 
 # implementing deserializer in product list as it should take data from user via post and update in db
-@api_view(['GET', 'POST'])
-def product_list(request):
-    if request.method == 'GET':
+
+class ProductList(APIView):
+    def get(self, request):
     # select_related makes site load faster as it loads the related field and makes less time to render so when we render product their collection also gets loaded
         queryset = Product.objects.select_related('collection').all()
         serializer = ProductSerializer(queryset, many = True, context={'request': request})
         return Response(serializer.data)
-
-    elif request.method == 'POST':
+     
+    def post(self, request):
         serializer = ProductSerializer(data = request.data)
         # validationg data
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+
+class ProductDetail(APIView):
+     
+    def get(self, request, id):
+        product = get_object_or_404(Product, pk=id)
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
+     
+    def put(self, request, id):
+        product = get_object_or_404(Product, pk=id)
+        serializer = ProductSerializer(product, data = request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+    
+    def delete(self, request, id):
+        product = get_object_or_404(Product, pk=id)
+        if product.orderitems.count() > 0:
+            return Response({'error': 'Product cannot be deleted because it is associated with order item'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
+        product.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+     
+
+
+# ThHIS CODE IS FUNCTION BASED IT IS CHANGED INTO CLASS BASED
+
+# @api_view(['GET', 'POST'])
+# def product_list(request):
+#     if request.method == 'GET':
+#     # select_related makes site load faster as it loads the related field and makes less time to render so when we render product their collection also gets loaded
+#         queryset = Product.objects.select_related('collection').all()
+#         serializer = ProductSerializer(queryset, many = True, context={'request': request})
+#         return Response(serializer.data)
+
+#     elif request.method == 'POST':
+#         serializer = ProductSerializer(data = request.data)
+#         # validationg data
+#         serializer.is_valid(raise_exception=True)
+#         serializer.save()
+#         return Response(serializer.data, status=status.HTTP_201_CREATED)
         
              
 
-@api_view(['GET', 'PUT', 'DELETE'])
-def product_detail(request, id):
-        product = get_object_or_404(Product, pk=id)
-        # retriving the data
-        if request.method == 'GET':
-            serializer = ProductSerializer(product)
-            return Response(serializer.data)
-        # to update the product
-        elif request.method == 'PUT':
-            serializer = ProductSerializer(product, data = request.data)
-            serializer.is_valid(raise_exception=True)
-            serializer.save()
-            return Response(serializer.data)
-        elif request.method == 'DELETE':
-            # here orderitems is related name in model OrderItem 
-            if product.orderitems.count() > 0:
-                  return Response({'error': 'Product cannot be deleted because it is associated with order item'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
-            product.delete()
-            return Response(status=status.HTTP_204_NO_CONTENT)        
+# @api_view(['GET', 'PUT', 'DELETE'])
+# def product_detail(request, id):
+#         product = get_object_or_404(Product, pk=id)
+#         # retriving the data
+#         if request.method == 'GET':
+#             serializer = ProductSerializer(product)
+#             return Response(serializer.data)
+#         # to update the product
+#         elif request.method == 'PUT':
+#             serializer = ProductSerializer(product, data = request.data)
+#             serializer.is_valid(raise_exception=True)
+#             serializer.save()
+#             return Response(serializer.data)
+#         elif request.method == 'DELETE':
+#             # here orderitems is related name in model OrderItem 
+#             if product.orderitems.count() > 0:
+#                   return Response({'error': 'Product cannot be deleted because it is associated with order item'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
+#             product.delete()
+#             return Response(status=status.HTTP_204_NO_CONTENT)        
 
 
 
